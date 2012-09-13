ba8433d1e688eeb7def2f00a9c30b7b0d68caaa2
James E. Butler <j.e.butler@limitpt.com>
2012-09-13 09:06:24 -0700

    Cleanup. Good to go on linux.

--- cmake_modules/FindSheaf.cmake
+++ cmake_modules/FindSheaf.cmake
@@ -4,30 +4,24 @@
 #
 #
 
-# RAW is going to need to work with sheaf libs in one of 2 ways: It's either going
-# to have a "dev" version; that is a built version in SHEAFSYSTEM_HOME/build, or a snapshot release
-# $$TODO: Equip RAW to deal with either -- export the "exported file" in the snapshot target
-
 #
 # Initialize the found variable
 #
 set(SHEAF_FOUND 0)
 
-#set(SHEAFSYSTEM_HOME $ENV{SHEAFSYSTEM_HOME} CACHE STRING "Sheaf Home Directory" FORCE)
-
 #
 # Find the exports file
 #
-message(STATUS "Looking for sheafSystem exports file ...")
+message(STATUS "Looking for SheafSystem exports file ...")
 find_file(SHEAFEXPORTSFILE SheafSystem-exports.cmake.in
-    HINTS ${SHEAFSYSTEM_HOME} ${SHEAFSYSTEM_HOME}/${CMAKE_BUILD_TYPE}/config ${SHEAFSYSTEM_HOME}/build)
+    HINTS ${SHEAFSYSTEM_HOME}/${CMAKE_BUILD_TYPE}/config ${SHEAFSYSTEM_HOME}/build)
 # Not good. The exports file wasn't where SHEAFSYSTEM_HOME claimed it was.    
 if(${SHEAFEXPORTSFILE} MATCHES "SHEAFEXPORTSFILE-NOTFOUND")
     message(FATAL_ERROR "SheafSystem-exports.cmake was not found; Is the SHEAFSYSTEM_HOME variable set correctly?")
 else()
     message(STATUS "Found ${SHEAFEXPORTSFILE}; configuring ...")
-    configure_file(${SHEAFEXPORTSFILE} ${CMAKE_BINARY_DIR}/SheafSystemExports.cmake)
-    include(${SHEAFEXPORTSFILE})
+    configure_file(${SHEAFEXPORTSFILE} ${CMAKE_BINARY_DIR}/SheafSystem-exports.cmake)
+    include(${CMAKE_BINARY_DIR}/SheafSystem-exports.cmake)
     set(SHEAF_FOUND 1)
 endif()
 
