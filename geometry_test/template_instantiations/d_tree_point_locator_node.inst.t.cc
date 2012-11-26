10e04218e9644eaef20d365c292cb7ae362e94f0
Bill Mason <wemason@limitpt.com>
2012-11-26 10:38:03 -0800

    Added a few more instantiations test cases. Currently just placeholders corresponding to moving template instantiation code in components/geometry/point_locators.

---
+++ geometry_test/template_instantiations/d_tree_point_locator_node.inst.t.cc
@@ -0,0 +1,48 @@
+// $RCSfile: xxx.inst.t.cc,v $ $Revision: 1.1.2.4 $ $Date: 2012/10/24 02:00:34 $
+
+//
+// Copyright (c) 2012 Limit Point Systems, Inc.
+//
+
+/// @example
+/// Unit test driver for class xxx.
+
+#include "xxx.h"
+
+// #include "geometry.h"
+#include "test_utils.h"
+
+using namespace geometry;
+
+namespace
+{
+
+
+
+} //end namespace
+
+int
+main(int xargc, char* xargv[])
+{
+  // Preconditions:
+
+  // Body:
+
+  print_header("Begin testing xxx_facet<???>:");
+
+  bool ltest = true;
+
+  //============================================================================
+
+  //============================================================================
+
+  int lresult = ltest ? EXIT_SUCCESS : EXIT_FAILURE;
+
+  print_footer("End testing xxx_facet<???>:");
+
+  // Postconditions:
+
+  // Exit:
+
+  return lresult;
+}
