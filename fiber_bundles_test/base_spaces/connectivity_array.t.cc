643227655d078506bd2c1c8818fa672b2204f64a
Bill Mason <wemason@limitpt.com>
2012-12-28 15:30:54 -0800

    Added one new unit test.

---
+++ fiber_bundles_test/base_spaces/connectivity_array.t.cc
@@ -0,0 +1,98 @@
+// $RCSfile: connectivity_array.t.cc,v $ $Revision: 1.1.2.7 $ $Date: 2012/10/19 02:23:27 $
+
+//
+// Copyright (c) 2012 Limit Point Systems, Inc.
+//
+
+/// Unit test driver for class connectivity_array.
+
+#include "connectivity_array.h"
+
+#include "std_iomanip.h"
+#include "std_iostream.h"
+#include "test_utils.h"
+
+using namespace fiber_bundle;
+
+namespace
+{
+  bool
+  test_connectivity_array_facet()
+  {
+    cout << boolalpha;
+    typedef connectivity_array::index_type index_type;
+
+    //connectivity_array();
+
+    connectivity_array larray;
+
+    //connectivity_array(index_type* xcol_cts,
+    //                   index_type xcol_cts_ub);
+
+    index_type lcol_cts[4] = {2, 2, 2, 2};
+    connectivity_array larray2(lcol_cts, 4);
+
+    //connectivity_array(index_type xrow_ct, index_type xvalue_ct);
+
+    connectivity_array larray3(4, 8);
+
+    //connectivity_array(const connectivity_array& xother);
+
+    connectivity_array larray_copy(larray2);
+
+    //virtual ~connectivity_array();
+
+    connectivity_array* larray_ptr = new connectivity_array(larray2);
+    delete larray_ptr; 
+
+    //virtual connectivity_array* clone() const;
+
+    connectivity_array* lclone = larray2.clone();
+    delete lclone; 
+
+    //virtual bool invariant() const;
+
+    bool linvariant = larray2.invariant();
+    cout << "linvariant = " << linvariant << endl; 
+
+    //virtual bool is_ancestor_of(const any* xother) const;
+
+   bool lis_ancestor_of = larray2.is_ancestor_of(&larray3);
+    cout << "lis_ancestor_of = " << lis_ancestor_of << endl; 
+
+    //scoped_index* types() const;
+
+    scoped_index* ltypes = larray2.types();
+
+    //scoped_index type(index_type xrow_index) const;
+
+    scoped_index ltype = larray2.type(2);
+
+    return true;
+  }
+}
+
+int
+main(int xargc, char* xargv[])
+{
+  // Preconditions:
+
+  // Body:
+
+  print_header("Begin testing connectivity_array");
+
+  bool ltest = true;
+
+  ltest &= test_connectivity_array_facet();
+
+  int lresult = ltest ? EXIT_SUCCESS : EXIT_FAILURE;
+
+  print_footer("End testing connectivity_array");
+
+  // Postconditions:
+
+  // Exit:
+
+  return lresult;
+}
+
