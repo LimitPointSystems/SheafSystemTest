7e21b81de064bef4d4be69fe31f74d386c6b7c81
Keith Bordonaro <kabordonaro@limitpt.com>
2013-01-16 16:30:17 -0500

    Files that didn't get added to the repository.

---
+++ sheaves_test/id_spaces/primary_index_space.t.cc
@@ -0,0 +1,67 @@
+// $RCSfile$ $Revision$ $Date$
+
+// $Name$
+//
+// Copyright (c) 2011 Limit Point Systems, Inc. 
+//
+
+/// @example primary_index_space.t.cc
+/// Test driver for primary id spaces.
+
+#include "assert_contract.h"
+#include "arg_list.h"
+#include "error_message.h"
+#include "index_space_family.h"
+#include "primary_index_space_handle.h"
+#include "primary_index_space_iterator.h"
+#include "primary_index_space_state.h"
+#include "std_iostream.h"
+#include "test_index_spaces.impl.h"
+
+using namespace sheaf;
+
+int main( int argc, char* argv[])
+{
+  // Preconditions:
+
+  // Body:
+
+  index_space_family lid_spaces;
+  pod_index_type lspace_id = lid_spaces.new_primary_state(6);
+
+  // Test handle facet.
+
+  test_handle_facet<primary_index_space_handle>(lid_spaces, lspace_id);
+
+  print_out_header("Testing miscellaneous handle functions");
+
+  primary_index_space_handle lhandle(lid_spaces, lspace_id);
+
+  cout << "offset()      = " << lhandle.offset() << endl;
+  cout << "extend(10)" << endl;
+  lhandle.extend(10);
+  cout << "hub_term_id() = " << lhandle.hub_term_id() << endl;
+
+  // Test iterator facet.
+
+  test_iterator_facet<primary_index_space_iterator>(lid_spaces, lspace_id);
+
+  print_out_header("Testing miscellaneous iterator functions");
+
+  primary_index_space_iterator litr(lid_spaces, lspace_id);
+
+  cout << "offset()             = " << litr.offset() << endl;
+  cout << "ct()                 = " << litr.ct() << endl;
+
+  // Test state facet.
+
+  test_state_facet(lid_spaces, lspace_id, 1, 1);
+
+  // Postconditions:
+
+  // Exit:
+
+  return 0;
+}
+
+  
