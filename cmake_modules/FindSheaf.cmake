16c538299ac37731965df964a30ebab2773ace4d
James E. Butler <jebutler@limitpoint.com>
2013-01-23 18:23:03 -0800

    OK, just kidding about the "last commit" thing. A few changes to findSheaf.

--- cmake_modules/FindSheaf.cmake
+++ cmake_modules/FindSheaf.cmake
@@ -14,21 +14,37 @@ set(SHEAF_FOUND 0)
 #
 message(STATUS "Looking for SheafSystem exports file ...")
 # Look for a binary installation of SheafSystem first
-if(EXISTS ${SHEAFSYSTEM_HOME}/config/${CMAKE_BUILD_TYPE}/SheafSystem-exports.cmake.in)
-    set(SHEAFEXPORTSFILE ${SHEAFSYSTEM_HOME}/config/${CMAKE_BUILD_TYPE}/SheafSystem-exports.cmake.in)
-    configure_file(${SHEAFEXPORTSFILE} ${CMAKE_BINARY_DIR}/SheafSystem-exports.cmake)
-    include(${CMAKE_BINARY_DIR}/SheafSystem-exports.cmake)
-    set(SHEAF_FOUND 1)
-# Didn't find install. Assume we are dealing with a build tree.    
-elseif(EXISTS ${SHEAFSYSTEM_HOME}/build/SheafSystem-exports.cmake)
-    set(SHEAFEXPORTSFILE ${SHEAFSYSTEM_HOME}/build/SheafSystem-exports.cmake CACHE STRING "Location of SheafSystem Exports file" FORCE)
-    include(${SHEAFEXPORTSFILE})
-    set(SHEAF_FOUND 1)
-# Didn't find either. Puke and exit.
+if(LINUX64GNU OR LINUX64INTEL)
+    if(EXISTS ${SHEAFSYSTEM_HOME}/${CMAKE_BUILD_TYPE}/config/SheafSystem-exports.cmake.in)
+        set(SHEAFEXPORTSFILE ${SHEAFSYSTEM_HOME}/${CMAKE_BUILD_TYPE}/config/SheafSystem-exports.cmake.in)
+        configure_file(${SHEAFEXPORTSFILE} ${CMAKE_BINARY_DIR}/SheafSystem-exports.cmake)
+        include(${CMAKE_BINARY_DIR}/SheafSystem-exports.cmake)
+        set(SHEAF_FOUND 1)
+    # Didn't find install. Assume we are dealing with a build tree.    
+    elseif(EXISTS ${SHEAFSYSTEM_HOME}/build/SheafSystem-exports.cmake)
+        set(SHEAFEXPORTSFILE ${SHEAFSYSTEM_HOME}/build/SheafSystem-exports.cmake CACHE STRING "Location of SheafSystem Exports file" FORCE)
+        include(${SHEAFEXPORTSFILE})
+        set(SHEAF_FOUND 1)
+    # Didn't find either. Puke and exit.
+    else()
+        message(FATAL_ERROR "SheafSystem exports file not found; Check the value of SHEAFSYSTEM_HOME.")
+    endif()  
 else()
-    message(FATAL_ERROR "SheafSystem exports file not found; Check the value of SHEAFSYSTEM_HOME.")
-endif()    
-
+    if(EXISTS ${SHEAFSYSTEM_HOME}/config/${CMAKE_BUILD_TYPE}/SheafSystem-exports.cmake.in)
+        set(SHEAFEXPORTSFILE ${SHEAFSYSTEM_HOME}/config/${CMAKE_BUILD_TYPE}/SheafSystem-exports.cmake.in)
+        configure_file(${SHEAFEXPORTSFILE} ${CMAKE_BINARY_DIR}/SheafSystem-exports.cmake)
+        include(${CMAKE_BINARY_DIR}/SheafSystem-exports.cmake)
+        set(SHEAF_FOUND 1)
+    # Didn't find install. Assume we are dealing with a build tree.    
+    elseif(EXISTS ${SHEAFSYSTEM_HOME}/build/SheafSystem-exports.cmake)
+        set(SHEAFEXPORTSFILE ${SHEAFSYSTEM_HOME}/build/SheafSystem-exports.cmake CACHE STRING "Location of SheafSystem Exports file" FORCE)
+        include(${SHEAFEXPORTSFILE})
+        set(SHEAF_FOUND 1)
+    # Didn't find either. Puke and exit.
+    else()
+        message(FATAL_ERROR "SheafSystem exports file not found; Check the value of SHEAFSYSTEM_HOME.")
+    endif()   
+endif()
    
 
 
