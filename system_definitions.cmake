05cfbc59b79b79ced33c944e739b6074b843ed79
James E. Butler <j.e.butler@limitpoint.com>
2013-05-01 07:29:53 -0700

    Backup.

--- system_definitions.cmake
+++ system_definitions.cmake
@@ -126,6 +126,15 @@ include(${CMAKE_MODULE_PATH}/target_declarations.cmake)
 #
 include(${CMAKE_MODULE_PATH}/find_prerequisites.cmake)
 
+#
+# C++11 features
+#
+message(STATUS " ")
+message(STATUS "Checking for C++11 Compliance - ")
+message(STATUS " ")
+
+include(${CMAKE_MODULE_PATH}/CheckCXX11Features.cmake)
+
 # If SHEAFSYSTEM_HOME contains white space, escape it.
 #string(REGEX REPLACE " " "\\\\ "  SHEAFSYSTEM_HOME "${SHEAFSYSTEM_HOME}")
 file(TO_NATIVE_PATH "${SHEAFSYSTEM_HOME}" SHEAFSYSTEM_HOME)
--- tools_test/component_definitions.cmake
+++ tools_test/component_definitions.cmake
@@ -24,9 +24,7 @@ include(CTest)
 #
 # Define the clusters for this component.
 #
-set(clusters SheafScope common/client_server common/event common/gui common/util 
-    viewer/animation viewer/common viewer/event viewer/render viewer/table 
-    viewer/user viewer/application)
+set(clusters examples)
 
 #
 # Initialize all variables for this component.
@@ -46,7 +44,7 @@ if(WIN64INTEL OR WIN64MSVC)
     #
     # Set the cumulative import library (win32) var for this component.
     #
-    set(${COMPONENT}_IMPORT_LIBS ${FIELDS_IMPORT_LIBS} ${${COMPONENT}_IMPORT_LIB} CACHE STRING " Cumulative import libraries (win32) for ${PROJECT_NAME}" FORCE)
+    set(${COMPONENT}_IMPORT_LIBS ${FIELDS_IMPORT_LIBS} ${${COMPONENT}_IMPORT_LIB} ${TOOLS_IMPORT_LIB} CACHE STRING " Cumulative import libraries (win32) for ${PROJECT_NAME}" FORCE)
 
 else()
 
@@ -60,6 +58,11 @@ else()
     #
     set(${COMPONENT}_SHARED_LIBS ${${COMPONENT}_SHARED_LIB} ${FIELDS_SHARED_LIB} ${FIELDS_TEST_SHARED_LIBS} CACHE STRING " Cumulative shared libraries for ${PROJECT_NAME}" FORCE)
 
+    #
+    # Set the cumulative Java binding library var for this component.
+    #
+    set(${COMPONENT}_JAVA_BINDING_LIBS ${${COMPONENT}_JAVA_BINDING_LIB} ${FIELDS_JAVA_BINDING_LIBS} CACHE STRING " Cumulative Java binding libraries for ${PROJECT_NAME}" FORCE)
+    
 endif()
 
 
@@ -130,4 +133,123 @@ function(add_library_targets)
         target_link_libraries(${${COMPONENT}_STATIC_LIB} ${FIELDS_STATIC_LIBS} ${JDK_LIBS} ${VTK_LIBS})
 
     endif()
+endfunction(add_library_targets)
+
+
+# END ORIGINAL TEXT
+
+#
+# Add the clusters to the project
+#
+add_clusters("${clusters}")
+
+if(WIN64INTEL OR WIN64MSVC)
+
+    #
+    # Set the cumulative import library (win32) var for this component.
+    #
+    set(${COMPONENT}_IMPORT_LIBS ${FIELDS_TEST_IMPORT_LIBS} ${${COMPONENT}_IMPORT_LIB} ${GEOMETRY_IMPORT_LIB} CACHE STRING "Cumulative import libraries (win32) for ${PROJECT_NAME}" FORCE)
+
+else()
+
+    #
+    # Set the cumulative shared library var for this component.
+    #
+    set(${COMPONENT}_STATIC_LIBS ${${COMPONENT}_STATIC_LIB} ${FIELDS_STATIC_LIBS} CACHE STRING " Cumulative static libraries for ${PROJECT_NAME}" FORCE)
+    
+    #
+    # Set the cumulative shared library var for this component.
+    #
+    set(${COMPONENT}_SHARED_LIBS ${${COMPONENT}_SHARED_LIB} libgeometry.so ${FIELDS_TEST_SHARED_LIBS} CACHE STRING " Cumulative shared libraries for ${PROJECT_NAME}" FORCE)
+    
+    #
+    # Set the cumulative Java binding library var for this component.
+    #
+    set(${COMPONENT}_JAVA_BINDING_LIBS ${${COMPONENT}_JAVA_BINDING_LIB} ${FIELDS_JAVA_BINDING_LIBS} CACHE STRING " Cumulative Java binding libraries for ${PROJECT_NAME}" FORCE)
+    
+    #
+    # Set the cumulative Python binding library var for this component.
+    #
+    set(${COMPONENT}_PYTHON_BINDING_LIBS ${${COMPONENT}_PYTHON_BINDING_LIB} ${FIELDS_PYTHON_BINDING_LIBS} CACHE STRING " Cumulative Python binding libraries for ${PROJECT_NAME}" FORCE)
+
+endif() 
+#
+# Set the cumulative include path for this component.
+#
+set(${COMPONENT}_IPATHS ${FIELDS_TEST_IPATHS} ${${COMPONENT}_IPATH} ${GEOMETRY_IPATH} CACHE STRING " Cumulative include paths for ${PROJECT_NAME}" FORCE)
+
+include_directories(${${COMPONENT}_IPATHS})
+
+#------------------------------------------------------------------------------
+# FUNCTION DEFINITION SECTION
+#------------------------------------------------------------------------------
+
+#
+# Create the library targets for this component.
+#
+function(add_library_targets)
+
+    if(${USE_VTK})
+        if(EXISTS ${VTK_LIB_DIR})
+            link_directories(${VTK_LIB_DIR})
+			include_directories(${VTK_INC_DIR})
+        else()
+            link_directories(${INSTALLED_VTK_LIB_DIR})
+        endif()
+    endif()
+
+     if(WIN64INTEL OR WIN64MSVC)
+
+        # Tell the linker where to look for this project's libraries.
+        link_directories(${${COMPONENT}_OUTPUT_DIR})
+
+        # Create the DLL.
+        add_library(${${COMPONENT}_DYNAMIC_LIB} SHARED ${${COMPONENT}_SRCS})
+        add_dependencies(${${COMPONENT}_DYNAMIC_LIB} ${FIELDS_TEST_IMPORT_LIBS} ${GEOMETRY_IMPORT_LIBS})
+
+        if(${USE_VTK})
+            target_link_libraries(${${COMPONENT}_DYNAMIC_LIB} ${FIELDS_IMPORT_LIBS} ${FIELDS_TEST_IMPORT_LIBS} ${VTK_LIBS}) 
+        else()
+            target_link_libraries(${${COMPONENT}_DYNAMIC_LIB} ${FIELDS_IMPORT_LIBS})        
+        endif() 
+     
+        set_target_properties(${${COMPONENT}_DYNAMIC_LIB} PROPERTIES FOLDER "Library Targets")
+        # Override cmake's placing of "${${COMPONENT}_DYNAMIC_LIB}_EXPORTS into the preproc symbol table.
+        set_target_properties(${${COMPONENT}_DYNAMIC_LIB} PROPERTIES DEFINE_SYMBOL "SHEAF_DLL_EXPORTS")
+
+    else() # Linux
+    
+        # Static library
+        add_library(${${COMPONENT}_STATIC_LIB} STATIC ${${COMPONENT}_SRCS})
+        add_dependencies(${${COMPONENT}_STATIC_LIB} ${FIELDS_STATIC_LIBS})
+        set_target_properties(${${COMPONENT}_STATIC_LIB} PROPERTIES OUTPUT_NAME ${PROJECT_NAME})
+
+         # Shared library
+        add_library(${${COMPONENT}_SHARED_LIB} SHARED ${${COMPONENT}_SRCS})
+        add_dependencies(${${COMPONENT}_SHARED_LIB} ${FIELDS_TEST_SHARED_LIBS} ${GEOMETRY_SHARED_LIBS})
+        set_target_properties(${${COMPONENT}_SHARED_LIB} PROPERTIES OUTPUT_NAME ${PROJECT_NAME} LINKER_LANGUAGE CXX)
+        set_target_properties(${${COMPONENT}_SHARED_LIB} PROPERTIES LINK_INTERFACE_LIBRARIES "") 
+        
+        # Override cmake's placing of "${COMPONENT_LIB}_EXPORTS into the preproc symbol table.
+        # CMake apparently detects the presence of cdecl_dllspec in the source and places
+        # -D<LIBRARY>_EXPORTS into the preproc symbol table no matter the platform.
+        set_target_properties(${${COMPONENT}_SHARED_LIB} PROPERTIES DEFINE_SYMBOL "")
+ 
+        # Define the library version.
+        set_target_properties(${${COMPONENT}_SHARED_LIB} PROPERTIES VERSION ${LIB_VERSION}) 
+    
+        # Library alias definitions
+        add_dependencies(${PROJECT_NAME}-shared-lib ${${COMPONENT}_SHARED_LIBS})
+        add_dependencies(${PROJECT_NAME}-static-lib ${${COMPONENT}_STATIC_LIBS})
+        
+        if(${USE_VTK})
+            target_link_libraries(${${COMPONENT}_SHARED_LIB} ${FIELDS_TEST_SHARED_LIBS} ${GEOMETRY_SHARED_LIBS} ${VTK_LIBS})
+            target_link_libraries(${${COMPONENT}_STATIC_LIB} ${FIELDS_STATIC_LIBS} ${VTK_LIBS})    
+        else()
+            target_link_libraries(${${COMPONENT}_SHARED_LIB} ${FIELDS_TEST_SHARED_LIBS} ${GEOMETRY_SHARED_LIBS})
+            target_link_libraries(${${COMPONENT}_STATIC_LIB} ${FIELDS_STATIC_LIBS})
+        endif()
+
+    endif()
+
 endfunction(add_library_targets)
