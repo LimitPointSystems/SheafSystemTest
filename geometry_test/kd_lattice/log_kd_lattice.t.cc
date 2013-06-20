70719fbff567032e03838299881a998d744e9280
Keith Bordonaro <kabordonaro@limitpt.com>
2013-06-20 09:58:48 -0400

    Commented out region insertion for given log files.  Until we name files with the same name as the structure log file, this code will always fail.

--- geometry_test/kd_lattice/log_kd_lattice.t.cc
+++ geometry_test/kd_lattice/log_kd_lattice.t.cc
@@ -74,150 +74,146 @@ int main( int argc, char* argv[])
     exit(1);
   }
 
-  lkd_lattice->display(false, true);
-  lkd_lattice->display_subvolumes(true, true);
+//   kd_bounding_box lbox(lkd_lattice->bounding_box());
+//   e3_lite lsize = lbox.size();
 
+//   if(lfile_name == "c2_simply_branching.log")
+//   {
+//     lbox.lb()[0] = 12500.0;
+//     lbox.lb()[1] = 16000.0;
+//     lbox.lb()[2] = 2000.0;
 
-  kd_bounding_box lbox(lkd_lattice->bounding_box());
-  e3_lite lsize = lbox.size();
+//     lbox.ub()[0] = 16200.0;
+//     lbox.ub()[1] = 24000.0;
+//     lbox.ub()[2] = 5000.0;
+//   }
+//   else if(lfile_name == "i4_cross-cutting.log")
+//   {
+//     lbox.lb()[0] = 19500.0;
+//     lbox.lb()[1] = 5800.0;
+//     lbox.lb()[2] = 2620.0;
 
-  if(lfile_name == "c2_simply_branching.log")
-  {
-    lbox.lb()[0] = 12500.0;
-    lbox.lb()[1] = 16000.0;
-    lbox.lb()[2] = 2000.0;
+//     lbox.ub()[0] = 22500.0;
+//     lbox.ub()[1] = 10000.0;
+//     lbox.ub()[2] = 4192.0;
+//   }
+//   else if(lfile_name == "salt_faults_h1.log")
+//   {
+//     //        lbox.lb()[0] = 4100.0;
+//     lbox.lb()[0] = 23600.0;
 
-    lbox.ub()[0] = 16200.0;
-    lbox.ub()[1] = 24000.0;
-    lbox.ub()[2] = 5000.0;
-  }
-  else if(lfile_name == "i4_cross-cutting.log")
-  {
-    lbox.lb()[0] = 19500.0;
-    lbox.lb()[1] = 5800.0;
-    lbox.lb()[2] = 2620.0;
+//     lbox.lb()[1] = 5100.0;
+//     //      lbox.lb()[1] = 9400.0;
 
-    lbox.ub()[0] = 22500.0;
-    lbox.ub()[1] = 10000.0;
-    lbox.ub()[2] = 4192.0;
-  }
-  else if(lfile_name == "salt_faults_h1.log")
-  {
-    //        lbox.lb()[0] = 4100.0;
-    lbox.lb()[0] = 23600.0;
+//     //      lbox.lb()[2] = -2500.0;
+//     lbox.lb()[2] = 500.0;
 
-    lbox.lb()[1] = 5100.0;
-    //      lbox.lb()[1] = 9400.0;
+//     //        lbox.ub()[0] = 30200.0;
+//     lbox.ub()[0] = 26700.0;
 
-    //      lbox.lb()[2] = -2500.0;
-    lbox.lb()[2] = 500.0;
+//     //      lbox.ub()[1] = 17900.0;
+//     //lbox.ub()[1] = 21000.0;
+//     lbox.ub()[1] = 26700.0;
 
-    //        lbox.ub()[0] = 30200.0;
-    lbox.ub()[0] = 26700.0;
+//     //      lbox.ub()[2] = 8500.0;
+//     lbox.ub()[2] = 5000.0;
+//   }
+//   else if(lfile_name == "salt_faults_h1_h4.log")
+//   {
+//     //        lbox.lb()[0] = 4100.0;
+//     lbox.lb()[0] = 23600.0;
 
-    //      lbox.ub()[1] = 17900.0;
-    //lbox.ub()[1] = 21000.0;
-    lbox.ub()[1] = 26700.0;
+//     lbox.lb()[1] = 5100.0;
+//     //      lbox.lb()[1] = 9400.0;
 
-    //      lbox.ub()[2] = 8500.0;
-    lbox.ub()[2] = 5000.0;
-  }
-  else if(lfile_name == "salt_faults_h1_h4.log")
-  {
-    //        lbox.lb()[0] = 4100.0;
-    lbox.lb()[0] = 23600.0;
+//     //      lbox.lb()[2] = -2500.0;
+//     lbox.lb()[2] = 500.0;
 
-    lbox.lb()[1] = 5100.0;
-    //      lbox.lb()[1] = 9400.0;
+//     //        lbox.ub()[0] = 30200.0;
+//     lbox.ub()[0] = 26700.0;
 
-    //      lbox.lb()[2] = -2500.0;
-    lbox.lb()[2] = 500.0;
+//     //      lbox.ub()[1] = 17900.0;
+//     //lbox.ub()[1] = 21000.0;
+//     lbox.ub()[1] = 26700.0;
 
-    //        lbox.ub()[0] = 30200.0;
-    lbox.ub()[0] = 26700.0;
+//     //      lbox.ub()[2] = 8500.0;
+//     lbox.ub()[2] = 5000.0;
+//   }
+//   else if(lfile_name == "wb_salt_faults_h1_h4.log")
+//   {
+//     //lbox.lb()[0] = 4100.0;
+//     lbox.lb()[0] = 7900.0;
+//     //lbox.lb()[0] = 20300.0;
+//     //lbox.lb()[0] = 23600.0;
 
-    //      lbox.ub()[1] = 17900.0;
-    //lbox.ub()[1] = 21000.0;
-    lbox.ub()[1] = 26700.0;
+//     lbox.lb()[1] = 5100.0;
+//     //      lbox.lb()[1] = 9400.0;
 
-    //      lbox.ub()[2] = 8500.0;
-    lbox.ub()[2] = 5000.0;
-  }
-  else if(lfile_name == "wb_salt_faults_h1_h4.log")
-  {
-    //lbox.lb()[0] = 4100.0;
-    lbox.lb()[0] = 7900.0;
-    //lbox.lb()[0] = 20300.0;
-    //lbox.lb()[0] = 23600.0;
+//     //      lbox.lb()[2] = -2500.0;
+//     lbox.lb()[2] = 500.0;
 
-    lbox.lb()[1] = 5100.0;
-    //      lbox.lb()[1] = 9400.0;
+//     //        lbox.ub()[0] = 30200.0;
+//     lbox.ub()[0] = 26700.0;
 
-    //      lbox.lb()[2] = -2500.0;
-    lbox.lb()[2] = 500.0;
+//     //      lbox.ub()[1] = 17900.0;
+//     //lbox.ub()[1] = 21000.0;
+//     lbox.ub()[1] = 26700.0;
 
-    //        lbox.ub()[0] = 30200.0;
-    lbox.ub()[0] = 26700.0;
+//     //      lbox.ub()[2] = 8500.0;
+//     //lbox.ub()[2] = 4772.0;
+//     lbox.ub()[2] = 5500.0;
+//   }
+//   else if(lfile_name == "F2_H1.log")
+//   {
+//     //        lbox.lb()[0] = 4100.0;
+//     lbox.lb()[0] = 23600.0;
 
-    //      lbox.ub()[1] = 17900.0;
-    //lbox.ub()[1] = 21000.0;
-    lbox.ub()[1] = 26700.0;
+//     //        lbox.lb()[1] = 5100.0;
+//     lbox.lb()[1] = 9400.0;
 
-    //      lbox.ub()[2] = 8500.0;
-    //lbox.ub()[2] = 4772.0;
-    lbox.ub()[2] = 5500.0;
-  }
-  else if(lfile_name == "F2_H1.log")
-  {
-    //        lbox.lb()[0] = 4100.0;
-    lbox.lb()[0] = 23600.0;
+//     lbox.lb()[2] = 1000.0;
 
-    //        lbox.lb()[1] = 5100.0;
-    lbox.lb()[1] = 9400.0;
+//     //        lbox.ub()[0] = 30200.0;
+//     lbox.ub()[0] = 26700.0;
 
-    lbox.lb()[2] = 1000.0;
+//     //        lbox.ub()[1] = 26700.0;
+//     lbox.ub()[1] = 21000.0;
 
-    //        lbox.ub()[0] = 30200.0;
-    lbox.ub()[0] = 26700.0;
+//     lbox.ub()[2] = 5000.0;
+//   }
+//   else if(lfile_name == "SB5_testdata.log")
+//   {
+//     //        lbox.lb()[0] = 4100.0;
+//     //lbox.lb()[0] = 20300.0;
+//     lbox.lb()[0] = 23600.0;
 
-    //        lbox.ub()[1] = 26700.0;
-    lbox.ub()[1] = 21000.0;
+//     //lbox.lb()[1] = 5100.0;
+//     lbox.lb()[1] = 9400.0;
 
-    lbox.ub()[2] = 5000.0;
-  }
-  else if(lfile_name == "SB5_testdata.log")
-  {
-    //        lbox.lb()[0] = 4100.0;
-    //lbox.lb()[0] = 20300.0;
-    lbox.lb()[0] = 23600.0;
+//     //      lbox.lb()[2] = -2500.0;
+//     lbox.lb()[2] = 500.0;
 
-    //lbox.lb()[1] = 5100.0;
-    lbox.lb()[1] = 9400.0;
+//     //        lbox.ub()[0] = 30200.0;
+//     lbox.ub()[0] = 26700.0;
 
-    //      lbox.lb()[2] = -2500.0;
-    lbox.lb()[2] = 500.0;
+//     //      lbox.ub()[1] = 17900.0;
+//     //lbox.ub()[1] = 21000.0;
+//     lbox.ub()[1] = 26700.0;
 
-    //        lbox.ub()[0] = 30200.0;
-    lbox.ub()[0] = 26700.0;
+//     //      lbox.ub()[2] = 8500.0;
+//     //lbox.ub()[2] = 4772.0;
+//     lbox.ub()[2] = 5500.0;
+//   }
+//   else
+//   {
+//     cout << "Don't have bounds for this file, quiting." << endl;
+//     return 0;
+//   }    
 
-    //      lbox.ub()[1] = 17900.0;
-    //lbox.ub()[1] = 21000.0;
-    lbox.ub()[1] = 26700.0;
+//   cout << endl << "domain: " << lkd_lattice->bounding_box() << endl;
+//   cout << "region of interest: " << lbox << endl;    
 
-    //      lbox.ub()[2] = 8500.0;
-    //lbox.ub()[2] = 4772.0;
-    lbox.ub()[2] = 5500.0;
-  }
-  else
-  {
-    cout << "Don't have bounds for this file, quiting." << endl;
-    return 0;
-  }    
-
-  cout << endl << "domain: " << lkd_lattice->bounding_box() << endl;
-  cout << "region of interest: " << lbox << endl;    
-
-  lkd_lattice->insert_region(lbox.lb(), lbox.ub());
+//   lkd_lattice->insert_region(lbox.lb(), lbox.ub());
     
   lkd_lattice->display(false, false);
 
