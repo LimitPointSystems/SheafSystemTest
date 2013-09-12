f3eefb337557bc5a26ccb38300e4c31fc666946d
jebutler <j.e.butler@limitpoint.com>
2013-09-12 12:00:30 -0700

    Backup. Added apache copyright to build system files.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,3 +1,19 @@
+#
+# Copyright (c) 2013 Limit Point Systems, Inc. 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 #=============================================================================
 # CMake - Cross Platform Makefile Generator
 # Copyright 2000-2009 Kitware, Inc., Insight Software Consortium
@@ -12,7 +28,7 @@
 set(CTEST_PROJECT_NAME "SheafSystem" CACHE STRING "")
 set(CTEST_NIGHTLY_START_TIME "8:00:00 UTC" CACHE STRING "")
 set(CTEST_DROP_METHOD "http" CACHE STRING "")
-set(CTEST_DROP_SITE "giantrat" CACHE STRING "")
+set(CTEST_DROP_SITE "www.limitpoint.com" CACHE STRING "")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=SheafSystem" CACHE STRING "")
 set(CTEST_DROP_SITE_CDASH TRUE CACHE BOOL "")
 set(CTEST_CDASH_VERSION "2.0.2" CACHE STRING "")
